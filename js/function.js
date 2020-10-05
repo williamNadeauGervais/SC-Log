ed2abd267a54ebe36468f1bda6edcc89b38a8d31
Will <william.nadeau.gervais@gmail.com>
2020-10-05 18:38:11 -0400

    version 1.7

--- js/function.js
+++ js/function.js
@@ -15,73 +15,85 @@ tips[10].addEventListener("click", ver11);
 tips[11].addEventListener("click", ver12);
 tips[12].addEventListener("click", ver13);
 tips[13].addEventListener("click", ver14);
+tips[14].addEventListener("click", ver15);
+tips[15].addEventListener("click", ver16);
 
 function ver1() {
     hideAll();
-    versions[13].style.display = "block";
+    versions[15].style.display = "block";
 }
 
 function ver2() {
     hideAll();
-    versions[12].style.display = "block";
+    versions[14].style.display = "block";
 }
 
 function ver3() {
     hideAll();
-    versions[11].style.display = "block";
+    versions[13].style.display = "block";
 }
 
 function ver4() {
     hideAll();
-    versions[10].style.display = "block";
+    versions[12].style.display = "block";
 }
 
 function ver5() {
     hideAll();
-    versions[9].style.display = "block";
+    versions[11].style.display = "block";
 }
 
 function ver6() {
     hideAll();
-    versions[8].style.display = "block";
+    versions[10].style.display = "block";
 }
 
 function ver7() {
     hideAll();
-    versions[7].style.display = "block";
+    versions[9].style.display = "block";
 }
 
 function ver8() {
     hideAll();
-    versions[6].style.display = "block";
+    versions[8].style.display = "block";
 }
 
 function ver9() {
     hideAll();
-    versions[5].style.display = "block";
+    versions[7].style.display = "block";
 }
 
 function ver10() {
     hideAll();
-    versions[4].style.display = "block";
+    versions[6].style.display = "block";
 }
 
 function ver11() {
     hideAll();
-    versions[3].style.display = "block";
+    versions[5].style.display = "block";
 }
 
 function ver12() {
     hideAll();
-    versions[2].style.display = "block";
+    versions[4].style.display = "block";
 }
 
 function ver13() {
     hideAll();
-    versions[1].style.display = "block";
+    versions[3].style.display = "block";
 }
 
 function ver14() {
+    hideAll();
+    versions[2].style.display = "block";
+}
+
+function ver15() {
+    hideAll();
+    versions[1].style.display = "block";
+}
+
+function ver16() {
     hideAll();
     versions[0].style.display = "block";
 }
